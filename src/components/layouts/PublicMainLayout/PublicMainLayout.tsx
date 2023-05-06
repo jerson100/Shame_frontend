1f52d2c21f186b17dcdd978b44b4f10ab39509d3
jerson100 <juamkoo@gmail.com>
2023-05-06 18:22:55 -0500

    Protegiendo las páginas privadas

---
+++ src/components/layouts/PublicMainLayout/PublicMainLayout.tsx
@@ -0,0 +1,31 @@
+import { Outlet } from "react-router-dom";
+import Header from "../../common/Header/Header";
+import useAuthContext from "../../../hooks/useAuthContext";
+import Loading from "../../common/Loading/Loading";
+import Sidebar from "../../common/Sidebar/Sidebar";
+import { useCallback, useState } from "react";
+
+const PublicMainLayout = () => {
+  const { loadingPrev } = useAuthContext();
+  const [showSidebar, setShowSidebar] = useState(false);
+  const handleShowSidebar = useCallback((state: boolean) => {
+    setShowSidebar(state);
+  }, []);
+  if (loadingPrev) return <Loading />;
+  return (
+    <div className="flex bg-zinc-50">
+      <Sidebar
+        handleShowSidebar={handleShowSidebar}
+        showSidebar={showSidebar}
+      />
+      <div className="flex-grow">
+        <Header handleShowSidebar={handleShowSidebar} />
+        <main className="mt-[50px] min-h-[calc(100vh-50px)] md:min-h-[calc(100vh-80px)] md:mt-0 overflow-auto p-4 flex flex-col">
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default PublicMainLayout;
--- src/components/routers/AppRouter/AppRouter.tsx
+++ src/components/routers/AppRouter/AppRouter.tsx
@@ -1,14 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NotFound from "../../common/NotFound";
 import LoginPage from "../../../pages/public/LoginPage";
-import HomePage from "../../../pages/private/HomePage";
+import HomePage from "../../../pages/public/HomePage";
 import PrivateMainLayout from "../../layouts/PrivateMainLayout";
 import LoginLayout from "../../layouts/LoginLayout";
 import CategoryPage from "../../../pages/public/CategoryPage";
 import SearchPage from "../../../pages/public/SearchPage";
 import UserProfilePage from "../../../pages/public/UserProfilePage";
-import CreatePinPage from "../../../pages/public/CreatePinPage";
+import CreatePinPage from "../../../pages/private/CreatePinPage";
 import DetailPinPage from "../../../pages/public/DetailPinPage";
+import PublicMainLayout from "../../layouts/PublicMainLayout/PublicMainLayout";
 
 const AppRouter = () => {
   return (
@@ -22,12 +23,14 @@ const AppRouter = () => {
             </LoginLayout>
           }
         />
-        <Route path="/" element={<PrivateMainLayout />}>
+        <Route path="/" element={<PublicMainLayout />}>
           <Route index element={<HomePage />} />
           <Route path="category/:category" element={<CategoryPage />} />
           <Route path="search" element={<SearchPage />} />
           <Route path="profile/:idUser" element={<UserProfilePage />} />
-          <Route path="create-pin" element={<CreatePinPage />} />
+          <Route path="create-pin" element={<PrivateMainLayout />}>
+            <Route index element={<CreatePinPage />} />
+          </Route>
           <Route path="pins/:idPin" element={<DetailPinPage />} />
         </Route>
         <Route path="*" element={<NotFound />} />
